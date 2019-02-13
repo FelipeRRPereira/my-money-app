24fb762d80bbabe4f5a5e8f47c6dff9c728b8ffd
Felipe Pereira <pereira.felipe@cwi.com.br>
2019-02-13 17:44:36 -0200

    Criado menu da aplicação e dashboard com cards de valores.

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,18 @@
 import '../common/tamplate/dependencies'
 import React from 'react'
 
+import Header from '../common/tamplate/header'
+import SideBar from '../common/tamplate/sideBar'
+import Footer from '../common/tamplate/footer'
+import Routes from './routes'
+
 export default props => (
     <div className='wrapper'>
-        <h1>App</h1>
+        <Header />
+        <SideBar />
+        <div className='content-wrapper'>
+            <Routes />
+        </div>
+        <Footer />
     </div>
 )

978eec59b3be628ce7dd727480d457a94c99a1d9
Felipe Pereira <pereira.felipe@cwi.com.br>
2019-02-19 11:16:05 -0300

    Finalizando criação das funcionalidades dos botões e abas do ciclo de pagamento.

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { getList } from './billingCycleActions'
+import { getList, showUpdate, showDelete } from './billingCycleActions'
 
 class billingCycleList extends Component {
     componentWillMount() {
@@ -14,6 +14,16 @@ class billingCycleList extends Component {
                 <td>{bc.name}</td>
                 <td>{bc.month}</td>
                 <td>{bc.year}</td>
+                <td>
+                    <button className='btn btn-warning' 
+                        onClick={() => this.props.showUpdate(bc)}>
+                        <i className='fa fa-pencil'></i>
+                    </button>
+                    <button className='btn btn-danger'
+                        onClick={() => this.props.showDelete(bc)}>
+                        <i className='fa fa-trash'></i>
+                    </button>
+                </td>
             </tr>
         ))
     }
@@ -27,6 +37,7 @@ class billingCycleList extends Component {
                             <th>Nome</th>
                             <th>Mês</th>
                             <th>Ano</th>
+                            <th className='table-actions'>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -39,5 +50,5 @@ class billingCycleList extends Component {
 }
 
 const mapStateToProps = state => ({list: state.billingCycle.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate, showDelete}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(billingCycleList);

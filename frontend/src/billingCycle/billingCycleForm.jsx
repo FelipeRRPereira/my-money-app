ac0f032a4beeba6907bb3a6921be92bf3e04f443
Felipe Pereira <pereira.felipe@cwi.com.br>
2019-02-25 14:32:22 -0300

    Adicionado cards de soma de creditos e debitos e ajustado menu.

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,11 +5,20 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions' 
 import labelAndInput from '../common/form/labelAndInput'
-import ItemList from './itemList';
+import ItemList from './itemList'
+import Summary from './summary'
 
 class billingCycleForm extends Component {
+    caculateSummary() {
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
     render() {
         const { handleSubmit, readOnly, credits, debts } = this.props
+        const { sumOfCredits, sumOfDebts } = this.caculateSummary() 
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
@@ -22,10 +31,11 @@ class billingCycleForm extends Component {
                     <Field name='year' component={labelAndInput} readOnly={readOnly}
                         label='Ano' cols='12 4' 
                         placeholder='Informe o ano' />
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly}
                         field='credits' legend='Créditos' />
                     <ItemList cols='12 6' list={debts} readOnly={readOnly}
-                        field='debts' legend='Créditos' />
+                        field='debts' legend='Débitos' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
